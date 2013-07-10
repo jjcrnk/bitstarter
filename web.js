2436144488eb2994aa3e218103c86c4852224e58
Jim Crnkovic <crnk@sbcglobal.net>
2013-07-10 12:55:01 +0000

    Added bufx buffsting to web.js

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
-
+var fs = require('fs');
+var bufx = new Buffer(30);
 var app = express.createServer(express.logger());
 
+bufx = fs.readFileSync("index.html");
+var buffstring = bufx.toString('utf-8');
+
+
+// console.log("Buffer contains " + bufx);
+// console.log("buffstring contains " + buffstring);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffstring);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
